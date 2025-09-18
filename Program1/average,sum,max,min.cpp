8e4ca578ce16af66ddbbb1594bb1f4118c6e61a4
aishwaryaps2002 <133131171+aishwaryaps2002@users.noreply.github.com>
2025-09-18 21:02:34 +0530

    average,sum,max,min.cpp

--- Program1/average,sum,max,min.cpp
+++ Program1/average,sum,max,min.cpp
@@ -1,57 +1,55 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 // Function prototypes
 int findSum(int arr[], int n);
 float findAverage(int arr[], int n);
 int findMax(int arr[], int n);
 int findMin(int arr[], int n);
-int main() 
-{
-    int arr[5], n, i;
-    printf("Enter number of integers (max 5): ");
-    scanf("%d", &n);
-    if(n < 1 || n > 5) 
-    {
-        printf("Invalid number! Please enter between 1 and 5.\n");
+
+int main() {
+    int arr[5], n;
+
+    cout << "Enter number of integers (max 5): ";
+    cin >> n;
+
+    if (n < 1 || n > 5) {
+        cout << "Invalid number! Please enter between 1 and 5." << endl;
         return 0;
     }
-    printf("Enter %d integers:\n", n);
-    for(i = 0; i < n; i++) 
-    {
-        scanf("%d", &arr[i]);
+
+    cout << "Enter " << n << " integers:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    printf("\nSum = %d", findSum(arr, n));
-    printf("\nAverage = %.2f", findAverage(arr, n));
-    printf("\nMaximum = %d", findMax(arr, n));
-    printf("\nMinimum = %d\n", findMin(arr, n));
+
+    cout << "\nSum = " << findSum(arr, n);
+    cout << "\nAverage = " << findAverage(arr, n);
+    cout << "\nMaximum = " << findMax(arr, n);
+    cout << "\nMinimum = " << findMin(arr, n) << endl;
+
     return 0;
 }
 
 // Function to find sum
-int findSum(int arr[], int n) 
-{
+int findSum(int arr[], int n) {
     int sum = 0;
-    for(int i = 0; i < n; i++) 
-    {
+    for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
     return sum;
 }
 
 // Function to find average
-float findAverage(int arr[], int n) 
-{
-    return (float)findSum(arr, n) / n;
+float findAverage(int arr[], int n) {
+    return static_cast<float>(findSum(arr, n)) / n;
 }
 
 // Function to find maximum
-int findMax(int arr[], int n) 
-{
+int findMax(int arr[], int n) {
     int max = arr[0];
-    for(int i = 1; i < n; i++) 
-    {
-        if(arr[i] > max) 
-        {
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
             max = arr[i];
         }
     }
@@ -59,15 +57,13 @@ int findMax(int arr[], int n)
 }
 
 // Function to find minimum
-int findMin(int arr[], int n) 
-{
+int findMin(int arr[], int n) {
     int min = arr[0];
-    for(int i = 1; i < n; i++) 
-    {
-        if(arr[i] < min) 
-        {
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
             min = arr[i];
         }
     }
     return min;
 }
+
